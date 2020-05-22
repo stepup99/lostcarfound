85eeb3fe569603648022abff81cf667cdeb425b9
anuj99 <mprince799@gmail.com>
2020-05-22 23:42:23 +0530

    code updated

--- server/server.js
+++ server/server.js
@@ -4,13 +4,10 @@ const app = express();
 const client = redis.createClient(6379, '127.0.0.1');
 
 client.on('connect', function () {
-    console.log("connected")
+    console.log("redis connected")
 });
 
 const PORT = 5000;
-
-
-
 
 
 app.listen(PORT, (req, res) => {
